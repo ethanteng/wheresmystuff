72d72bbf92063e00ce9afc58dd9033b7ff67d9ca
Ethan Teng <ethanteng@gmail.com>
2020-07-17 14:46:08 -0700

    more readable date & time

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import json
 import requests
 import MySQLdb
 import config
+import datetime
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -11,9 +13,10 @@ app = Flask(__name__)
 def respond():
 	update = request.json
 	tracking_code = update["result"]["tracking_code"]
-	status = update["result"]["status"]
-	est_delivery_date = update["result"]["est_delivery_date"]
-	carrier = update["result"]["carrier"]
+	status = str(update["result"]["status"])
+	est_delivery_date_str = str(update["result"]["est_delivery_date"])
+	est_delivery_date = datetime.strptime(est_delivery_date_str,"%m/%d/%Y %H:%M")
+	carrier = str(update["result"]["carrier"])
 
 	send_email(tracking_code, status, est_delivery_date, carrier)
 	
@@ -30,14 +33,14 @@ def send_email(tracking_code, status, est_delivery_date, carrier):
 	cursor.execute(query_packages, [tracking_code])
 	result = cursor.fetchone()
 	user_id = result[0]
-	description = result[1]
+	description = str(result[1])
 	
 	# Get user name and email
 	query_users = """SELECT firstname, lastname, email FROM users WHERE id = %s"""
 	cursor.execute(query_users, [user_id])
 	user = cursor.fetchone()
-	firstname = user[0]
-	lastname = user[1]
+	firstname = str(user[0])
+	lastname = str(user[1])
 	email = str(user[2])
 	
 	# Send email
@@ -48,5 +51,5 @@ def send_email(tracking_code, status, est_delivery_date, carrier):
 			data={"from": "Support at WheresMyStuff<support@sandbox6441ed402cbe4179802eb8bf0af5d96d.mailgun.org>",
 				"to": email,
 				"bcc": "ethanteng@gmail.com",
-				"subject": "Update about your " + str(description),
-				"text": "Tracking code: " + str(tracking_code) + "\n" + "Delivery status: " + str(status) + "\n" + "Estimated delivery date: " + str(est_delivery_date) + "\n" + "Carrier: " + str(carrier)})
+				"subject": "Update about your " + description,
+				"text": "Tracking code: " + str(tracking_code) + "\n" + "Delivery status: " + status + "\n" + "Estimated delivery date: " + est_delivery_date + "\n" + "Carrier: " + carrier})
