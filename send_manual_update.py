73c6adebe139c0044800fbb45b71b69b74894cb1
Ethan Teng <ethanteng@gmail.com>
2020-07-31 22:09:27 +0000

    live manual update

--- send_manual_update.py
+++ send_manual_update.py
@@ -73,7 +73,7 @@ def send_email(tracking_code, status, status_detail, est_delivery_date, carrier,
 
 
 # Manually entered values
-tracking_code = "EZ1000000001"
+tracking_code = "783361"
 status = "delayed - will call to schedule delivery"
 status_detail = "waiting for another item to arrive (Latta Beach Sand Dining Table for 8)"
 est_delivery_date = datetime.date(2020, 8, 13)
@@ -83,6 +83,6 @@ destination = "SAN FRANCISCO, CA"
 current_city = "SAN FRANCISCO"
 current_state = "CA"
 current_country = "US"
-public_url = None
+public_url = "https://www.article.com/login?next=%2Faccount"
 
 send_email(tracking_code, status, status_detail, est_delivery_date, carrier, origin, destination, current_city, current_state, current_country, public_url)
