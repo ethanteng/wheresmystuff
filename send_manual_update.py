9573f82fd04bf353f2d190bf50d023373260a594
ethanteng <ethanteng@gmail.com>
2020-08-01 03:33:46 +0530

    manual updates

--- send_manual_update.py
+++ send_manual_update.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import email_helper
 import datetime
+import MySQLdb
+import config
 
 def send_email(tracking_code, status, status_detail, est_delivery_date, carrier, origin, destination, current_city, current_state, current_country, public_url):
 	# Setup MySQL Connection
@@ -71,10 +73,10 @@ def send_email(tracking_code, status, status_detail, est_delivery_date, carrier,
 
 
 # Manually entered values
-tracking_code = "783361"
+tracking_code = "EZ1000000001"
 status = "delayed - will call to schedule delivery"
 status_detail = "waiting for another item to arrive (Latta Beach Sand Dining Table for 8)"
-est_delivery_date = str(datetime.date(2020, 8, 13).strftime("%b %d %Y %-I:%M%p"))
+est_delivery_date = datetime.date(2020, 8, 13)
 carrier = "City Business 415-900-9118"
 origin = None
 destination = "SAN FRANCISCO, CA"
@@ -82,3 +84,5 @@ current_city = "SAN FRANCISCO"
 current_state = "CA"
 current_country = "US"
 public_url = None
+
+send_email(tracking_code, status, status_detail, est_delivery_date, carrier, origin, destination, current_city, current_state, current_country, public_url)
