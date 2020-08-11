d3a272d6c7c76f5643ab2244b51dcbb9ec232332
Ethan Teng <ethanteng@gmail.com>
2020-08-11 10:10:27 -0700

    modified startup script to add crawlera cert

--- startup.sh
+++ startup.sh
@@ -4,5 +4,7 @@ python3 -m pip install requests
 python3 -m pip install lxml
 python3 -m pip install bs4
 python3 -m pip install python-dateutil
+sudo cp /home/ethanteng_gmail_com/crawlera-ca.crt /usr/local/share/ca-certificates/crawlera-ca.crt
+sudo update-ca-certificates
 export FLASK_APP=/home/ethanteng_gmail_com/wheresmystuff/main.py
 python3 -m flask run
